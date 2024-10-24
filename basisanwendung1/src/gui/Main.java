18c28cd0ef87cf1702b618640af95465bf7c03ff
sohaa <sohaa@soha>
2024-10-24 18:40:46 +0200

    bitte schönnnnnnnnn

--- basisanwendung1/src/gui/Main.java
+++ basisanwendung1/src/gui/Main.java
@@ -1,8 +1,17 @@
 package gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("guten tag");
-	}
 
+
+public class Main extends Application {
+	
+    	@Override
+    	public void start(Stage primaryStage) {
+        	new Anwendersystem(primaryStage);
+    	}	
+    
+    	public static void main(String[] args){ 
+	  	launch(args);
+    	}
 }
